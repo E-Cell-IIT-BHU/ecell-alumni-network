92a6a96fb89e6159b632679bf618a48d8099a389
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2022-08-14 14:52:15 +0530

    required changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import React from "react";
-import  Navigation  from "./navbar";
+import Navigation from "./navbar";
 import Intro from "./Intro";
 import About from "./About";
 import Grid from "./Grid";
-import Team from "./Team"
+import Team from "./Team";
 import ContactUs from "./contactUs";
 import Form from "./form";
 import Footer from "./Footer";
 
-
 function App() {
   return (
     <div className="App">
-    <Navigation />
-    <Intro />
-
-    <div style={{textAlign:"center"}}>
-    
-    <About />
-    <Grid />
-    <Team />
-    <ContactUs />
-    <Form/>
-    <Footer/>
-    
-    <hr style={{margin:"2% 20%"}}></hr>
-    </div>
+      <div style={{ textAlign: "center" }}>
+        <Navigation />
+        <Intro />
+        <About />
+        <Grid />
+        <Team />
+        <ContactUs />
+        <Form />
+        <Footer />
+        <hr style={{ margin: "2% 20%" }}></hr>
+      </div>
     </div>
   );
 }
-
 
 export default App;
