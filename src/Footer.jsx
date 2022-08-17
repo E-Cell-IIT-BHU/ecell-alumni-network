75dafd52457ec487874d18a1463eb79df172f07d
ecelliitbhu <87716183+ecelliitbhu@users.noreply.github.com>
2022-08-17 22:11:31 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -77,7 +77,7 @@ const Footer = () => {
                     <a href="https://ecelliitbhu.com" target="_blank">E-Cell Home</a>
                   </li>
                   <li>
-                    <a href="/forms/raiseFunds">Raise Funds</a>
+                    <a href="/raise_fund">Raise Funds</a>
                   </li>
                   <li>
                     <a
